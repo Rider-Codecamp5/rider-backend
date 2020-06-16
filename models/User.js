4f8d6bcc0c7639195fe0564ff4b66921414a186c
xeusteerapat <xeus085@gmail.com>
2020-06-16 14:38:31 +0700

    init branch

--- models/User.js
+++ models/User.js
@@ -1,32 +1,34 @@
-// module.exports = (sequelize, DataTypes) => {
-//     const User = sequelize.define('User', {
-//         profile_pic: {
-//             type: DataTypes.STRING
-//         },
-//         first_name: {
-//             type: DataTypes.STRING
-//         },
-//         last_name: {
-//             type: DataTypes.STRING
-//         },
-//         address: {
-//             type: DataTypes.STRING
-//         },
-//         email: {
-//             type: DataTypes.STRING,
-//         },
-//         password: {
-//             type: DataTypes.STRING
-//         }
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      profile_pic: {
+        type: DataTypes.STRING,
+      },
+      first_name: {
+        type: DataTypes.STRING,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+      },
+      address: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
 
-//     }, {
-//         timestamps: false
-//     })
+  User.associate = models => {
+    User.belongToMany(models.report, { through: 'Report' });
+  };
 
-//     User.associate = models => {
-//         User.belongToMany(models.report, { through: 'Report' })
-
-//     }
-
-//     return User;
-// }
+  return User;
+};
