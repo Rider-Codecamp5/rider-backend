307612ad7d0f43b0ad4e83a1429ccf7f5804099a
wichaisw <wichai.sw19@gmail.com>
2020-06-19 21:48:57 +0700

    add driver offerRoute

--- controllers/Driver.js
+++ controllers/Driver.js
@@ -3,7 +3,7 @@ const db = require('../models');
 const registerDriver = async (req, res) => {
     const id = req.params.userId;
     const driver_license = req.body.driver_license;
-    const seat = req.body.seat;
+    const seating_capacity = req.body.seating_capacity;
     const car_model = req.body.car_model;
     const car_color = req.body.car_color;
     const bank_account = req.body.bank_account;
@@ -11,7 +11,7 @@ const registerDriver = async (req, res) => {
     const body = {
         id,
         driver_license,
-        seat,
+        seating_capacity,
         car_model,
         car_color,
         bank_account,
@@ -36,22 +36,36 @@ const deleteDriver = async (req, res) => {
         res.status(400).send({ message: 'Invalid driver ID' })
     }
 }
+
 const offerRoute = async (req, res) => {
+    let userData = await req.user;
     const origin = req.body.origin;
     const destination = req.body.destination;
-    // const date_time = req.body.date + req.body.time;
-    const luggage = req.body.lugggae;
-    const seating_capacity = req.body.seat;
+    const date_time = req.body.date + " " + req.body.time;
+    const luggage = req.body.luggage;
+    const seating_capacity = req.body.seating_capacity;
     const price = req.body.price;
 
     const body = {
         from: origin,
         to: destination,
-        // date_time,
+        date_time,
         luggage,
         seating_capacity,
-        price
+        price,
+        status: "available"
     }
+
+    await db.driver.update(
+        body,
+        { where: { id:  userData.id} }
+    )
+
+    res.status(201).json({
+        message: "created new route",
+        driver: userData.id,
+        details: body
+    })
 }
 
 module.exports = { registerDriver, deleteDriver, offerRoute }
