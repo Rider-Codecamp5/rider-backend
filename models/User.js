ec4a9bea8a8df73944c46cb42d419006da10096c
wichaisw <wichai.sw19@gmail.com>
2020-06-23 13:18:00 +0700

    add phone_number to user model

--- models/User.js
+++ models/User.js
@@ -18,6 +18,9 @@ module.exports = (sequelize, DataTypes) => {
     address: {
       type: DataTypes.STRING
     },
+    phone_number: {
+      type: DataTypes.STRING
+    }
   })
 
   user.associate = models => {
