17bd313ce059d112a638586765fbaeb8ed6ff779
xeusteerapat <xeus085@gmail.com>
2020-06-27 18:38:24 +0700

    update Driver model add confirmation column

--- models/Driver.js
+++ models/Driver.js
@@ -1,72 +1,75 @@
 module.exports = (sequelize, DataTypes) => {
-    const driver = sequelize.define('driver', {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: false,
-            primaryKey: true
-        },
-        driver_license : {
-            type : DataTypes.STRING,
-        },
-        seat : {
-            type : DataTypes.INTEGER,
-        },
-        car_model : {
-            type : DataTypes.STRING,
-        },
-        car_color: {
-            type: DataTypes.STRING,
-        },
-        bank_account : {
-            type : DataTypes.STRING,
-        },
-        from : {
-            type: DataTypes.STRING,
-        },
-        to : {
-            type: DataTypes.STRING,
-        },
-        from_lat: {
-            type: DataTypes.FLOAT,
-        },
-        from_lng: {
-            type: DataTypes.FLOAT,
-        },
-        to_lat: {
-            type: DataTypes.FLOAT,
-        },
-        to_lng: {
-            type: DataTypes.FLOAT,
-        },
-        date_time : {
-            type: DataTypes.STRING,
-        },
-        seating_capacity: {
-            type: DataTypes.INTEGER,
-        },
-        luggage: {
-            type : DataTypes.BOOLEAN,
-        },
-        price : {
-            type: DataTypes.STRING,
-        },
-        booked_seat : {
-            type: DataTypes.INTEGER,
-        },
-        status : {
-            type: DataTypes.STRING,
-        },
-        cancel_reason : {
-            type: DataTypes.STRING,
-        }
-    })
+  const driver = sequelize.define('driver', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: false,
+      primaryKey: true,
+    },
+    driver_license: {
+      type: DataTypes.STRING,
+    },
+    seat: {
+      type: DataTypes.INTEGER,
+    },
+    car_model: {
+      type: DataTypes.STRING,
+    },
+    car_color: {
+      type: DataTypes.STRING,
+    },
+    bank_account: {
+      type: DataTypes.STRING,
+    },
+    from: {
+      type: DataTypes.STRING,
+    },
+    to: {
+      type: DataTypes.STRING,
+    },
+    from_lat: {
+      type: DataTypes.FLOAT,
+    },
+    from_lng: {
+      type: DataTypes.FLOAT,
+    },
+    to_lat: {
+      type: DataTypes.FLOAT,
+    },
+    to_lng: {
+      type: DataTypes.FLOAT,
+    },
+    date_time: {
+      type: DataTypes.STRING,
+    },
+    seating_capacity: {
+      type: DataTypes.INTEGER,
+    },
+    luggage: {
+      type: DataTypes.BOOLEAN,
+    },
+    price: {
+      type: DataTypes.STRING,
+    },
+    booked_seat: {
+      type: DataTypes.INTEGER,
+    },
+    status: {
+      type: DataTypes.STRING,
+    },
+    confirmation: {
+      type: DataTypes.STRING,
+    },
+    cancel_reason: {
+      type: DataTypes.STRING,
+    },
+  });
 
-    driver.associate = models => {
-        driver.belongsTo(models.user, {
-            foreignKey: "passenger_id",
-        });
-        driver.hasMany(models.trip_history);
-    }
+  driver.associate = models => {
+    driver.belongsTo(models.user, {
+      foreignKey: 'passenger_id',
+    });
+    driver.hasMany(models.trip_history);
+  };
 
-    return driver
-}
+  return driver;
+};
