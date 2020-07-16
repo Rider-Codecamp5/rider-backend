45b854709417e216a99d8011bdba20959f5f7082
xeusteerapat <xeus085@gmail.com>
2020-07-16 20:22:20 +0700

    fix bug payment controller

--- controllers/TripHistory.js
+++ controllers/TripHistory.js
@@ -1,31 +1,31 @@
 const db = require('../models');
 
-const getAllPassengerTrip = async(req, res)  => {
+const getAllPassengerTrip = async (req, res) => {
   let userId = await req.user.id;
   let history = await db.trip_history.findAll({
     where: {
       passenger_id: userId,
-    }
-  })
+    },
+  });
 
   res.status(200).json({
     message: 'success getting all trip as a passenger',
     history,
-  })
-}
+  });
+};
 
-const getAllDriverTrip = async(req, res)  => {
+const getAllDriverTrip = async (req, res) => {
   let userId = await req.user.id;
   let history = await db.trip_history.findAll({
     where: {
       driver_id: userId,
-    }
-  })
+    },
+  });
   res.status(200).json({
     message: 'success getting all trip as a driver',
     history,
-  })
-}
+  });
+};
 
 const getRecentTrip = async (req, res) => {
   let passengerId = await req.user.id;
@@ -33,14 +33,14 @@ const getRecentTrip = async (req, res) => {
     where: {
       passenger_id: passengerId,
     },
-    order: [['date_time', 'DESC']]
-  })
+    order: [['date_time', 'DESC']],
+  });
 
   res.status(200).json({
     message: 'success getting the lastest trip',
     selectedHistory,
-  })
-}
+  });
+};
 
 const saveTrip = async (req, res) => {
   let userData = await req.user;
@@ -54,7 +54,7 @@ const saveTrip = async (req, res) => {
     },
     {
       where: {
-        passenger_id: userData.id,
+        driver_id: driverId,
       },
     }
   );
@@ -62,4 +62,9 @@ const saveTrip = async (req, res) => {
   res.status(201).json({ message: 'Trip hitory save to database' });
 };
 
-module.exports = { getAllDriverTrip, getAllPassengerTrip, getRecentTrip, saveTrip };
+module.exports = {
+  getAllDriverTrip,
+  getAllPassengerTrip,
+  getRecentTrip,
+  saveTrip,
+};
